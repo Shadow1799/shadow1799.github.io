7e1957ed397655afb643f0f510ea248707941df8
Shadow1799 <rubberlittle13@gmail.com>
2024-05-16 23:50:25 -0400

    dark mode works ?

--- js/day-night.js
+++ js/day-night.js
@@ -2,10 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('day/night').addEventListener('change', function() {
-    if (this.checked) {
-      function(('link').attr('href', 'styles_dark.css'));
-    } else {
-      function(('link').attr('href', 'styles.css'));
-    }
+    toggleTheme()
+
+     function toggleTheme() {
+       var theme = document.getElementsByTagName('link')[0];
+
+            if (theme.getAttribute('href') == 'css/styles.css') {
+               theme.setAttribute('href', 'css/styles_dark.css');
+           }
+           else {
+               theme.setAttribute('href', 'css/styles.css');
+           }
+
+
+     }
   });
 });
