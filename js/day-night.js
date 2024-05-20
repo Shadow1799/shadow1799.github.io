c4e48d70eaf2626b46bf15de7ed74cb28c0bb6ca
Shadow1799 <rubberlittle13@gmail.com>
2024-05-20 10:56:20 -0400

    changed to a more modern design
    still working on making responsive but this is the shell for my new website upgrade

--- js/day-night.js
+++ js/day-night.js
@@ -1,16 +1,1 @@
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('day/night').addEventListener('change', function() {
-    toggleTheme()
-     function toggleTheme() {
-       var theme = document.getElementsByTagName('link')[0];
-            if (theme.getAttribute('href') == 'css/styles_light.css') {
-               theme.setAttribute('href', 'css/styles_dark.css');
-           }
-           else {
-               theme.setAttribute('href', 'css/styles_light.css');
-           }
-     }
-  });
-});
